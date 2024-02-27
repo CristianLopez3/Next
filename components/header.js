d0264e3afc6296197d8cc3feb675e2ae10dc2888
Cristian <cristian.c.lopez.m@gmail.com>
2024-02-26 19:44:14 -0500

    paramsm in url

---
+++ components/header.js
@@ -0,0 +1,7 @@
+export default function Header(){
+  return(
+    <>
+      <h2>Header of my app</h2>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,33 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "system-ui"],
+      serif: ["Poppins", "Georgia"],
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        "3xl": "0px -12px 191px -43px rgba(0,0,0,0.75)",
+      },
+      colors: {
+        primary: "#ff8700",
+        secondary: "#101010",
+        grayDark: "#3D3B40",
+        grayLight: "rgb(242 243 248)",
+        third: "#ff8710",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
       },
     },
   },
