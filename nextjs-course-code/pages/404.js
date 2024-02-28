6c819dc2ecb06cb06401bfcf20867bcf1ac18322
Cristian <cristian.c.lopez.m@gmail.com>
2024-02-28 17:30:02 -0500

    not found page

---
+++ nextjs-course-code/pages/404.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>Not found page</h1>
+      <Link href="/">Go to home</Link>
+    </>
+  );
+};
+
+export default NotFound;
--- nextjs-course-code/pages/client/[id]/index.js
+++ nextjs-course-code/pages/client/[id]/index.js
@@ -1,9 +1,17 @@
+import { useRouter } from "next/router";
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+  const loadProjectHandler = () => {
+    //load data
+    router.push("/clients/max/projecta");
+  };
   return (
     <>
       <h1>The projects of a Given client id</h1>
+      <button onClick={loadProjectHandler}>Load Project to</button>
     </>
-  )
-}
+  );
+};
 
 export default ClientProjectsPage;
